258528d6c64a9a7667ac839d5a9ecdbb6beb7f57
yamoo9 <yamoo9@naver.com>
2023-08-07 14:13:15 +0900

    선언형 프로그래밍
    - 배열의 map 메서드 활용
    - 기능(함수) 선언

--- src/FP/chapters/00-programming-paradigm.js
+++ src/FP/chapters/00-programming-paradigm.js
@@ -20,17 +20,14 @@ const courses = [
   },
 ];
 
-console.log('원본 데이터\n', courses);
-
-// 1. 과정 배열을 순환하여 각 과정 이름의 좌우 공백 제거
-// 2. 과정 배열을 순환하여 각 과정 이름 대문자화
+// console.log('원본 데이터\n', courses);
 
 // ES2015(v6)
 // [전개구문(spread syntax)]을 사용하면 배열을 복사할 수 있다.
 // 얕은 복사 (shallow copy)
 const updateCourses = [] // [...courses];
 
-// [기능 1.] 좌우 공백 제거
+// 1. 과정 배열을 순환하여 각 과정 이름의 좌우 공백 제거
 // 명령형으로 프로그래밍 한다.
 // C, JAVA 문법
 // for문
@@ -40,19 +37,22 @@ for(let i=0, l=courses.length; i<l; i = i + 1) {
   // 얕은 복사 (shallow copy)
   const course = { ...courses[i] };
   course.name = course.name.trim();
-  updateCourses.push(course);
+  updateCourses.push(course); // [{ ...}, { ... }]
 }
 
-// [기능 2.] 대문자화
+// 2. 과정 배열을 순환하여 각 과정 이름 대문자화
 for(let i=0, l=updateCourses.length; i<l; ++i) {
   const course = updateCourses[i];
   course.name = course.name.toUpperCase();
 }
 
 
-console.log('변형된 데이터\n', updateCourses);
+// console.log('업데이트 데이터\n', updateCourses);
 
-console.assert(!Object.is(courses, updateCourses), '🚨 courses와 updateCourses는 동일한 객체이다.');
+// console.assert(
+//   !Object.is(courses, updateCourses), 
+//   '🚨 courses와 updateCourses는 동일한 객체이다.'
+// );
 
 // --------------------------------------------------------------------------
 // 선언형 프로그래밍
@@ -68,9 +68,46 @@ const subjects = [
   },
 ];
 
+console.log('원본 데이터\n', subjects);
+
 // 1. 객체 이름(name) 속성 좌우 공백 제거 함수 선언
+function toTrim(object) {
+  const o = {...object};
+  o.name = o.name.trim();
+  return o;
+}
+
+// console.log(toTrim(subjects[0]));
+// console.log(toTrim(subjects[1]));
+
 // 2. 객체 이름(name) 속성 대문자화 함수 선언
+function toUpperCase(object) {
+  const o = {...object};
+  o.name = o.name.toUpperCase();
+  return o;
+}
+
+// console.log(toUpperCase(subjects[0]));
+// console.log(toUpperCase(subjects[1]));
+
 // 3. 과목 이름 "좌우 공백 제거" → "대문자화" 후, 새로운 과목 배열 생성
+// ES5의 map()을 사용해야 한다.
+// - 조건 1. 새로운 배열 반환
+// - 조건 2. 배열 순환 후, 기능 처리(적용)
+
+/* const updateSubjects = subjects.map(subject => {
+  const copySubject = toTrim(subject);
+  return copySubject;
+}).map(subject=> {
+  const copySubject = toUpperCase(subject);
+  return copySubject;
+}) */
+
+const updateSubjects = subjects
+                        .map(toTrim)
+                        .map(toUpperCase);
+
+console.log('업데이트 데이터\n', updateSubjects);
 
 
 // --------------------------------------------------------------------------
