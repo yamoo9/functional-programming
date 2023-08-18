b45226aa8c8f0d22e745e97a7a6fe973db03173d
yamoo9 <yamoo9@naver.com>
2023-08-19 05:35:50 +0900

    명령형 vs. 선언형 프로그래밍 스타일

--- src/FP/chapters/08-jquery-react.jsx
+++ src/FP/chapters/08-jquery-react.jsx
@@ -3,40 +3,52 @@
 // --------------------------------------------------------------------------
 // - 명령형 vs. 선언형 프로그래밍 스타일
 // --------------------------------------------------------------------------
-/*@jsx h */
-import { createElement as h, createRoot, useState } from '../../library/y9';
-
-
-// --------------------------------------------------------------------------
-// 명령형 프로그래밍 스타일
-// - DOM 스크립트 (Vanilla)
-// - jQuery 라이브러리 (https://jquery.com)
-
-// eslint-disable-next-line no-undef
-// jQuery(($) => {
-//   console.log($.fn.version);
-// })
-
 
 // --------------------------------------------------------------------------
 // 선언형 프로그래밍 스타일
 // - React 라이브러리 (https://react.dev)
 // - React 모방 라이브러리 (lib/y9.js)
 
-function App() {
+/* @jsx h */
+import { createElement as h, createRoot, useState } from '@/library/y9';
 
-  const [ count, setCount ] = useState(10);
+const initialHeadline = 'React 모방 라이브러리';
+
+function App() {
+  const [headline, setHeadline] = useState(initialHeadline);
+  const [count, setCount] = useState(10);
 
   return (
     <div>
-      <h1>React 아님</h1>
-      <button onClick={() => setCount(count + 1)}>
+      <h2>{headline}</h2>
+      <button type="button" onClick={() => {
+        setCount(count + 1);
+        setHeadline(headline.includes(initialHeadline) ? 'React Mimic Library' : initialHeadline);
+      }}>
         {count}
       </button>
     </div>
-  )
+  );
 }
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+createRoot(document.getElementById('root')).render(<App />);
+
+// --------------------------------------------------------------------------
+// 명령형 프로그래밍 스타일
+// - DOM 스크립트 (Vanilla)
+// - jQuery 라이브러리 (https://jquery.com)
+jQuery(($) => {
+  const initialHeadline = 'jQuery 라이브러리';
+  let headline = initialHeadline;
+  let count = 10;
+
+  $('#jQuery')
+    .html('<div />')
+    .find('div')
+    .append(`<h2>${headline}</h2>`)
+    .append(`<button type="button">${count}</button>`)
+    .on('click', 'button', (e) => {
+      headline = headline === initialHeadline ? 'jQuery Library' : initialHeadline;
+      $(e.target).text(++count).prev().text(headline);
+    });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 const viteConfig = defineConfig({
   server: {
     host: 'localhost',
     port: 8000,
     cors: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 });
 
