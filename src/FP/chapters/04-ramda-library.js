1bd3efdb09f933cb4abc96ca4e0b117e1580d89a
yamoo9 <yamoo9@naver.com>
2023-08-18 20:14:14 +0900

    람다 라이브러리로 커링 활용

--- src/FP/chapters/04-ramda-library.js
+++ src/FP/chapters/04-ramda-library.js
@@ -4,24 +4,29 @@
 // - https://ramdajs.com
 // --------------------------------------------------------------------------
 
+import { curry } from 'ramda';
 
-const friends = '재성 현수 수진 정희 수환'.split(' ');
 
+const friends = '승민 은빈 다인 수완 동균 선용 치현 규민 소이 현주 규정 명화 수빈 현주 소희 동호 명현 이호 민성 진주'.split(' ');
 
 // --------------------------------------------------------------------------
 // 일반 함수 → 커링 함수
-let greet = (greeting, name) => `${greeting} ${name}`;
+let greet = curry((greeting, name) => `${greeting} ${name}`);
 
 
 // --------------------------------------------------------------------------
 // 전문화(specialize) 함수 생성
-// - morningGreet
-// - afternoonGreet
-// - eveningGreet
+const happyNewYearGreet = greet('올 해도 행복하길!');
+const morningGreet = greet('좋은 아침!!');
+const afternoonGreet = greet('맛있는 런치 타임 가져~')
+const eveningGreet = greet('우아한 밤이되길!')
 
 
 // --------------------------------------------------------------------------
 // 고차 함수 map 활용
-// - friendMorninigGreeting
-// - friendAfternoonGreeting
-// - friendEveningGreeting
+const friendHappyNewYearGreeting = friends.map(happyNewYearGreet);
+const friendMorninigGreeting = friends.map(morningGreet);
+const friendAfternoonGreeting = friends.map(afternoonGreet);
+const friendEveningGreeting = friends.map(eveningGreet);
+
+friendHappyNewYearGreeting.forEach((greetMessage) => console.log(greetMessage));
